92135b3888637a9f6ad74cf5ba4dc1e57e1f554a
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2020-11-15 20:29:51 -0300

    compiler: .c -> .cpp

--- src/compiler/expropta.cpp
+++ src/compiler/expropta.cpp
@@ -2,4 +2,4 @@
  * However it produces a slightly different code if used in
  * macro compiler (there is an additional parameter passed to some functions)
  */
-#include "hbexpra.c"
+#include "hbexpra.cpp"
