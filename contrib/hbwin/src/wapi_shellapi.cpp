98a26b10cba15ea0f20b9d30a1719591468575aa
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2021-10-11 22:41:11 -0300

    Update hbwin

--- contrib/hbwin/src/wapi_shellapi.cpp
+++ contrib/hbwin/src/wapi_shellapi.cpp
@@ -85,7 +85,8 @@ HB_FUNC( WAPI_SHELLEXECUTE_WAIT )
    void * hDirectory;
    BOOL retVal;
    MSG msg;
-   SHELLEXECUTEINFO ShExecInfo = {0};
+   SHELLEXECUTEINFO ShExecInfo;
+   memset( &ShExecInfo, 0, sizeof( ShExecInfo ) );
    ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
    ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
    ShExecInfo.hwnd = static_cast< HWND >( hb_parptr( 1 ) );
