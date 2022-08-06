2c26d3a43ff8ec76b60bf27560118829938961cd
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2022-08-05 22:47:19 -0300

    Update arc4.cpp Fix error with sysctl.h

--- src/rtl/arc4.cpp
+++ src/rtl/arc4.cpp
@@ -54,7 +54,7 @@
     * of runtime C libraries, like musl, doesn't even expose it. Here we
     * look for it only with "classic" line of libc's.
     */
-#  if (!defined(HB_OS_LINUX) || (defined(__GLIBC__) || defined(__UCLIBC__)))
+#  if (!defined(HB_OS_LINUX) || ((defined(__GLIBC__) && !((__GLIBC__ > 2) || ((__GLIBC__ == 2) && (__GLIBC_MINOR__ >= 30))))) || defined(__UCLIBC__))
 #     define HAVE_SYS_SYSCTL_H
 #  endif
 #  define HAVE_DECL_CTL_KERN
