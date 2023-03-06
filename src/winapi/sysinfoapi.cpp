4e173b792202d46099ef749932aa09b42f742f21
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-03-06 05:15:32 -0300

    Add sysinfoapi.cpp

---
+++ src/winapi/sysinfoapi.cpp
@@ -0,0 +1,53 @@
+/*
+
+  WINAPI for Harbour++ - Bindings libraries for Harbour++ and WINAPI
+
+  Copyright (C) 2023 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+
+*/
+
+/*
+MIT License
+
+Copyright (c) 2023 Marcos Antonio Gambeta
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+  NOTE: source code generated with the help of a code generator
+*/
+
+#include <windows.h>
+#include <sysinfoapi.h>
+#include "hbapi.hpp"
+#include "hbapiitm.hpp"
+#include "hbapicls.hpp"
+#include "hbwinuni.hpp"
+#include "winapi.hpp"
+
+/*
+void GetSystemTime(
+  [out] LPSYSTEMTIME lpSystemTime
+);
+*/
+HB_FUNC( WINAPI_GETSYSTEMTIME )
+{
+  GetSystemTime(static_cast<LPSYSTEMTIME>(winapi_get_ptr(1)));
+}
