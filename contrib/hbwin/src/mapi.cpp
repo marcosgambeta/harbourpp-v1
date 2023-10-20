a0faa5d8803a6f45855a4fcec837043057bed1a9
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-10-20 15:40:25 -0300

    Update hbwin

--- contrib/hbwin/src/mapi.cpp
+++ contrib/hbwin/src/mapi.cpp
@@ -48,11 +48,11 @@
 #include "hbapi.hpp" /* for HB_OS_* detection */
 
 #if defined(UNICODE)
-#  define HB_UNICODE_ORI
+#define HB_UNICODE_ORI
 #endif
 
 #if defined(UNICODE)
-#   undef UNICODE
+#undef UNICODE
 #endif
 
 #include "hbwapi.hpp"
@@ -60,163 +60,147 @@
 #include <mapi.h>
 
 #if !defined(MAPI_RECEIPT_REQUESTED)
-#  if defined(MAPI_RECIPIENT_REQUESTED)
-#     define MAPI_RECEIPT_REQUESTED  MAPI_RECIPIENT_REQUESTED
-#  else
-#     define MAPI_RECEIPT_REQUESTED  0x00000002L
-#  endif
+#if defined(MAPI_RECIPIENT_REQUESTED)
+#define MAPI_RECEIPT_REQUESTED  MAPI_RECIPIENT_REQUESTED
+#else
+#define MAPI_RECEIPT_REQUESTED  0x00000002L
+#endif
 #endif
 
 #if defined(__CYGWIN__)
-using LPMAPISENDMAIL = ULONG(PASCAL *)(LHANDLE, ULONG, lpMapiMessage, FLAGS, ULONG);
+using LPMAPISENDMAIL = ULONG (PASCAL *)(LHANDLE, ULONG, lpMapiMessage, FLAGS, ULONG);
 #endif
 
 HB_FUNC( WIN_MAPISENDMAIL )
 {
+   HINSTANCE hMapiDll;
+
    /* Set default return value */
    hb_retnl(-1);
 
-   PHB_ITEM pFrom     = hb_param(8, Harbour::Item::ARRAY);
-   PHB_ITEM pRecpList = hb_param(9, Harbour::Item::ARRAY);
-   PHB_ITEM pFileList = hb_param(10, Harbour::Item::ARRAY);
+   if( (hMapiDll = hbwapi_LoadLibrarySystem(
+#if defined(HB_UNICODE_ORI) || defined(UNICODE)
+            reinterpret_cast<LPCTSTR>(L"mapi32.dll")
+#else
+            static_cast<LPCTSTR>("mapi32.dll")
+#endif
+            )) >= reinterpret_cast<HINSTANCE>(32) ) {
+      LPMAPISENDMAIL MAPISendMail = reinterpret_cast<LPMAPISENDMAIL>(reinterpret_cast<void*>(HB_WINAPI_GETPROCADDRESS(hMapiDll, "MAPISendMail")));
 
-   HB_SIZE nRecpCount = pRecpList ? hb_arrayLen(pRecpList) : 0;
-   HB_SIZE nFileCount = pFileList ? hb_arrayLen(pFileList) : 0;
-   HB_SIZE i;
+      if( MAPISendMail ) {
+         PHB_ITEM pFrom     = hb_param(8, HB_IT_ARRAY);
+         PHB_ITEM pRecpList = hb_param(9, HB_IT_ARRAY);
+         PHB_ITEM pFileList = hb_param(10, HB_IT_ARRAY);
 
-   void ** hString;
-   int     iString = 0;
+         HB_SIZE nRecpCount = pRecpList ? hb_arrayLen(pRecpList) : 0;
+         HB_SIZE nFileCount = pFileList ? hb_arrayLen(pFileList) : 0;
 
-   MapiMessage   note{};
-   MapiRecipDesc origin{};
-   FLAGS         flags;
+         int iString = 0;
 
-   hString = static_cast<void**>(hb_xgrab((4 + 2 + (2 * nRecpCount) + (2 * nFileCount)) * sizeof(void*)));
+         void ** hString = static_cast<void**>(hb_xgrab((4 + 2 + (2 * nRecpCount) + (2 * nFileCount)) * sizeof(void*)));
 
-   note.lpszSubject      = const_cast<LPSTR>(HB_PARSTR(1, &hString[iString++], nullptr));
-   note.lpszNoteText     = const_cast<LPSTR>(HB_PARSTR(2, &hString[iString++], nullptr));
-   note.lpszMessageType  = const_cast<LPSTR>(HB_PARSTR(3, &hString[iString++], nullptr));
-   note.lpszDateReceived = const_cast<LPSTR>(HB_PARSTRDEF(4, &hString[iString++], nullptr));
+         MapiMessage note{};
+         note.lpszSubject      = const_cast<LPSTR>(HB_PARSTR(1, &hString[iString++], nullptr));
+         note.lpszNoteText     = const_cast<LPSTR>(HB_PARSTR(2, &hString[iString++], nullptr));
+         note.lpszMessageType  = const_cast<LPSTR>(HB_PARSTR(3, &hString[iString++], nullptr));
+         note.lpszDateReceived = const_cast<LPSTR>(HB_PARSTRDEF(4, &hString[ iString++], nullptr));
 
-   if( nRecpCount )
-   {
-      note.lpRecips = static_cast<MapiRecipDesc*>(hb_xgrab(nRecpCount * sizeof(MapiRecipDesc)));
-      memset(note.lpRecips, 0, nRecpCount * sizeof(MapiRecipDesc));
-   }
+         if( nRecpCount ) {
+            note.lpRecips = static_cast<MapiRecipDesc*>(hb_xgrab(nRecpCount * sizeof(MapiRecipDesc)));
+            memset(note.lpRecips, 0, nRecpCount * sizeof(MapiRecipDesc));
+         }
 
-   if( nFileCount )
-   {
-      note.lpFiles = static_cast<MapiFileDesc*>(hb_xgrab(nFileCount * sizeof(MapiFileDesc)));
-      memset(note.lpFiles, 0, nFileCount * sizeof(MapiFileDesc));
-   }
+         if( nFileCount ) {
+            note.lpFiles = static_cast<MapiFileDesc*>(hb_xgrab(nFileCount * sizeof(MapiFileDesc)));
+            memset(note.lpFiles, 0, nFileCount * sizeof(MapiFileDesc));
+         }
 
-   if( hb_parl(6) )
-   {
-      note.flFlags |= MAPI_RECEIPT_REQUESTED;
-   }
+         if( hb_parl(6) ) {
+            note.flFlags |= MAPI_RECEIPT_REQUESTED;
+         }
 
-   flags = MAPI_LOGON_UI;
+         FLAGS flags = MAPI_LOGON_UI;
 
-   if( hb_parl(7) )
-   {
-      flags |= MAPI_DIALOG;
-   }
+         if( hb_parl(7) ) {
+            flags |= MAPI_DIALOG;
+         }
 
-   if( pFrom && hb_arrayLen(pFrom) >= 2 )
-   {
-      origin.lpszName     = const_cast<LPSTR>(HB_ARRAYGETSTR(pFrom, 1, &hString[iString++], nullptr));
-      origin.lpszAddress  = const_cast<LPSTR>(HB_ARRAYGETSTR(pFrom, 2, &hString[iString++], nullptr)); /* optional */
-      origin.ulRecipClass = MAPI_ORIG;
+         MapiRecipDesc origin{};
 
-      note.lpOriginator = &origin;
-   }
-   else if( HB_ISCHAR(8) )
-   {
-      origin.lpszName     = const_cast<LPSTR>(HB_PARSTR(8, &hString[iString++], nullptr));
-      origin.ulRecipClass = MAPI_ORIG;
+         if( pFrom && hb_arrayLen(pFrom) >= 2 ) {
+            origin.lpszName     = const_cast<LPSTR>(HB_ARRAYGETSTR(pFrom, 1, &hString[iString++], nullptr));
+            origin.lpszAddress  = const_cast<LPSTR>(HB_ARRAYGETSTR(pFrom, 2, &hString[iString++], nullptr)); /* optional */
+            origin.ulRecipClass = MAPI_ORIG;
+            note.lpOriginator = &origin;
+         } else if( HB_ISCHAR(8) ) {
+            origin.lpszName     = const_cast<LPSTR>(HB_PARSTR(8, &hString[iString++], nullptr));
+            origin.ulRecipClass = MAPI_ORIG;
+            note.lpOriginator = &origin;
+         }
 
-      note.lpOriginator = &origin;
-   }
+         for( HB_SIZE i = 0; i < nRecpCount; ++i ) {
+            PHB_ITEM pItem = hb_arrayGetItemPtr(pRecpList, i + 1);
 
-   for( i = 0; i < nRecpCount; ++i )
-   {
-      PHB_ITEM pItem = hb_arrayGetItemPtr(pRecpList, i + 1);
+            if( HB_IS_ARRAY(pItem) && hb_arrayLen(pItem) >= 2 ) {
+               if( hb_arrayGetCLen(pItem, 1) > 0 ) {
+                  note.lpRecips[note.nRecipCount].lpszName = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 1, &hString[iString++], nullptr));
 
-      if( HB_IS_ARRAY(pItem) && hb_arrayLen(pItem) >= 2 )
-      {
-         if( hb_arrayGetCLen(pItem, 1) > 0 )
-         {
-            note.lpRecips[note.nRecipCount].lpszName = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 1, &hString[iString++], nullptr));
+                  if( hb_arrayGetCLen(pItem, 2) > 0 ) {
+                     note.lpRecips[note.nRecipCount].lpszAddress = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 2, &hString[iString++], nullptr));
+                  }
+               } else if( hb_arrayGetCLen(pItem, 2) > 0 ) {
+                  note.lpRecips[note.nRecipCount].lpszName = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 2, &hString[iString++], nullptr));
+               } else {
+                  continue;
+               }
 
-            if( hb_arrayGetCLen(pItem, 2) > 0 )
-            {
-               note.lpRecips[note.nRecipCount].lpszAddress = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 2, &hString[iString++], nullptr));
+               if( hb_arrayLen(pItem) >= 3 && HB_IS_NUMERIC(hb_arrayGetItemPtr(pItem, 3)) ) {
+                  note.lpRecips[note.nRecipCount].ulRecipClass = static_cast<ULONG>(hb_arrayGetNL(pItem, 3));
+               } else {
+                  note.lpRecips[note.nRecipCount].ulRecipClass = MAPI_TO;
+               }
+
+               ++note.nRecipCount;
+            } else if( HB_IS_STRING(pItem) ) {
+               note.lpRecips[note.nRecipCount].lpszName     = const_cast<LPSTR>(HB_ITEMGETSTR(pItem, &hString[iString++], nullptr));
+               note.lpRecips[note.nRecipCount].ulRecipClass = MAPI_TO;
+
+               ++note.nRecipCount;
             }
          }
-         else if( hb_arrayGetCLen(pItem, 2) > 0 )
-         {
-            note.lpRecips[note.nRecipCount].lpszName = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 2, &hString[iString++], nullptr));
-         }
-         else
-         {
-            continue;
-         }
 
-         if( hb_arrayLen(pItem) >= 3 && HB_IS_NUMERIC(hb_arrayGetItemPtr(pItem, 3)))
-         {
-            note.lpRecips[note.nRecipCount].ulRecipClass = static_cast<ULONG>(hb_arrayGetNL(pItem, 3));
-         }
-         else
-         {
-            note.lpRecips[note.nRecipCount].ulRecipClass = MAPI_TO;
+         for( HB_SIZE i = 0; i < nFileCount; ++i ) {
+            PHB_ITEM pItem = hb_arrayGetItemPtr(pFileList, i + 1);
+
+            if( HB_IS_ARRAY(pItem) && hb_arrayLen(pItem) >= 1 && hb_arrayGetCLen(pItem, 1) > 0 ) {
+               note.lpFiles[note.nFileCount].lpszPathName = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 1, &hString[iString++], nullptr ));
+               note.lpFiles[note.nFileCount].lpszFileName = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 2, &hString[iString++], nullptr)); /* optional */
+               note.lpFiles[note.nFileCount].nPosition    = static_cast<ULONG>(-1);
+               ++note.nFileCount;
+            } else if( HB_IS_STRING(pItem) ) {
+               note.lpFiles[note.nFileCount].lpszPathName = const_cast<LPSTR>(HB_ITEMGETSTR(pItem, &hString[iString++], nullptr));
+               note.lpFiles[note.nFileCount].nPosition    = static_cast<ULONG>(-1);
+               ++note.nFileCount;
+            }
          }
 
-         ++note.nRecipCount;
+         hb_retnint((*MAPISendMail)(0, reinterpret_cast<ULONG_PTR>(GetActiveWindow()), &note, flags, 0));
+
+         if( nRecpCount > 0 ) {
+            hb_xfree(note.lpRecips);
+         }
+
+         if( nFileCount > 0 ) {
+            hb_xfree(note.lpFiles);
+         }
+
+         while( --iString >= 0 ) {
+            hb_strfree(hString[iString]);
+         }
+
+         hb_xfree(hString);
       }
-      else if( HB_IS_STRING(pItem) )
-      {
-         note.lpRecips[note.nRecipCount].lpszName     = const_cast<LPSTR>(HB_ITEMGETSTR(pItem, &hString[iString++], nullptr));
-         note.lpRecips[note.nRecipCount].ulRecipClass = MAPI_TO;
 
-         ++note.nRecipCount;
-      }
+      FreeLibrary(hMapiDll);
    }
-
-   for( i = 0; i < nFileCount; ++i )
-   {
-      PHB_ITEM pItem = hb_arrayGetItemPtr(pFileList, i + 1);
-
-      if( HB_IS_ARRAY(pItem) && hb_arrayLen(pItem) >= 1 && hb_arrayGetCLen(pItem, 1) > 0 )
-      {
-         note.lpFiles[note.nFileCount].lpszPathName = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 1, &hString[iString++], nullptr));
-         note.lpFiles[note.nFileCount].lpszFileName = const_cast<LPSTR>(HB_ARRAYGETSTR(pItem, 2, &hString[iString++], nullptr)); /* optional */
-         note.lpFiles[note.nFileCount].nPosition    = static_cast<ULONG>(-1);
-         ++note.nFileCount;
-      }
-      else if( HB_IS_STRING(pItem) )
-      {
-         note.lpFiles[note.nFileCount].lpszPathName = const_cast<LPSTR>(HB_ITEMGETSTR(pItem, &hString[iString++], nullptr));
-         note.lpFiles[note.nFileCount].nPosition    = static_cast<ULONG>(-1);
-         ++note.nFileCount;
-      }
-   }
-
-   hb_retnint(MAPISendMail(0, reinterpret_cast<ULONG_PTR>(GetActiveWindow()), &note, flags, 0));
-
-   if( nRecpCount > 0 )
-   {
-      hb_xfree(note.lpRecips);
-   }
-
-   if( nFileCount > 0 )
-   {
-      hb_xfree(note.lpFiles);
-   }
-
-   while( --iString >= 0 )
-   {
-      hb_strfree(hString[iString]);
-   }
-
-   hb_xfree(hString);
 }
