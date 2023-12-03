15dbd1d7f00815768f06ea63d8a77369af8f5229
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2023-12-03 06:13:03 -0300

    Changes from Harbour Project https://github.com/harbour/core/commit/d12558f0cae6798418961523d45280cfa6a63fe3

--- src/common/hbdate.cpp
+++ src/common/hbdate.cpp
@@ -535,24 +535,28 @@ HB_BOOL hb_timeStrGet(const char * szTime, int * piHour, int * piMinutes, int * 
                }
             }
          }
-         while( HB_ISSPACE(*szTime) ) {
+         if( iBlocks > 0 && (szTime[0] == 'Z' || szTime[0] == 'z') ) {
             ++szTime;
-         }
-         if( (szTime[0] == 'p' || szTime[0] == 'P') && (szTime[1] == 'm' || szTime[1] == 'M') ) {
-            ++iBlocks;
-            szTime += 2;
-            if( iHour == 0 ) {
-               iHour = 24;    /* wrong time */
-            } else if( iHour != 12 ) {
-               iHour += 12;
+         } else {
+            while( HB_ISSPACE(*szTime) ) {
+               ++szTime;
             }
-         } else if( (szTime[0] == 'a' || szTime[0] == 'A') && (szTime[1] == 'm' || szTime[1] == 'M') ) {
-            ++iBlocks;
-            szTime += 2;
-            if( iHour == 0 ) {
-               iHour = 24;    /* wrong time */
-            } else if( iHour == 12 ) {
-               iHour = 0;
+            if( (szTime[0] == 'p' || szTime[0] == 'P' ) && (szTime[1] == 'm' || szTime[1] == 'M') ) {
+               ++iBlocks;
+               szTime += 2;
+               if( iHour == 0 ) {
+                  iHour = 24;    /* wrong time */
+               } else if( iHour != 12 ) {
+                  iHour += 12;
+               }
+            } else if( (szTime[0] == 'a' || szTime[0] == 'A' ) && (szTime[1] == 'm' || szTime[1] == 'M') ) {
+               ++iBlocks;
+               szTime += 2;
+               if( iHour == 0 ) {
+                  iHour = 24;    /* wrong time */
+               } else if( iHour == 12 ) {
+                  iHour = 0;
+               }
             }
          }
          while( HB_ISSPACE(*szTime) ) {
