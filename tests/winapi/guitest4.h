fca316d8abad875ff789f46ea7059a5c69f757aa
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-01-25 14:28:35 -0300

    Add guitest4.prg

---
+++ tests/winapi/guitest4.h
@@ -0,0 +1,11 @@
+/*
+ * WinApi test
+ *
+ * Copyright (c) 2024 Marcos Antonio Gambeta <marcosgambeta AT outlook DOT com>
+ *
+ */
+
+#define IDR_MYMENU 101
+#define IDM_FILE_NEW  1
+#define IDM_FILE_OPEN 2
+#define IDM_FILE_QUIT 3
