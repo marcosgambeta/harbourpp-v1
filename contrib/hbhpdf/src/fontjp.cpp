3b2198ef7f4e1804f1189a1c92a46d2f0cb6b07d
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2024-12-24 04:41:27 -0300

    Update contrib Add $HB_BEGIN_LICENSE$ and $HB_END_LICENSE$ Work in progress

--- contrib/hbhpdf/src/fontjp.cpp
+++ contrib/hbhpdf/src/fontjp.cpp
@@ -2,6 +2,7 @@
 // Copyright 2008 Pritpal Bedi <pritpal@vouchcac.com>
 //
 
+// $HB_BEGIN_LICENSE$
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2, or (at your option)
@@ -40,6 +41,7 @@
 // If you write modifications of your own for Harbour, it is your choice
 // whether to permit this exception to apply to your modifications.
 // If you do not wish that, delete this exception notice.
+// $HB_END_LICENSE$
 
 #include "hbhpdf.hpp"
 
