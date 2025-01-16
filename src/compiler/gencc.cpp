69af58be3a1293ab42ba6ff9f2f6bae24ebf185d
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-01-16 05:08:36 -0300

    Update gencc.cpp Change '/* *** END PROC *** */' to '// END PROC'

--- src/compiler/gencc.cpp
+++ src/compiler/gencc.cpp
@@ -448,7 +448,7 @@ static HB_GENC_FUNC(hb_p_endblock)
 static HB_GENC_FUNC(hb_p_endproc)
 {
   HB_GENC_LABEL();
-  fprintf(cargo->yyc, "\t/* *** END PROC *** */\n");
+  fprintf(cargo->yyc, "\t// END PROC\n");
   if (nPCodePos < pFunc->nPCodePos - 1)
   {
     if (cargo->iNestedBlock)
