73cff3cbe4ed4cd67a9476041c2a8f6ef20637a2
Marcos Antonio Gambeta <5998677+marcosgambeta@users.noreply.github.com>
2025-09-10 16:14:11 -0300

    Add zig.mk and zig64.mk: work in progress

---
+++ config/win/zig.mk
@@ -0,0 +1,95 @@
+ifeq ($(HB_BUILD_MODE),cpp)
+   HB_CMP := zig c++
+else
+   HB_CMP := zig cc
+endif
+
+OBJ_EXT := .o
+LIB_PREF := lib
+LIB_EXT := .a
+
+HB_DYN_COPT := -DHB_DYNLIB
+
+CC := $(HB_CCPATH)$(HB_CMP)$(HB_CCSUFFIX)
+ifneq ($(filter --analyze, $(HB_USER_CFLAGS)),)
+   CC_IN :=
+else
+   CC_IN := -c
+endif
+CC_OUT := -o
+
+CFLAGS += -I. -I$(HB_HOST_INC) -D__ZIGCOMPILER__
+
+ifneq ($(HB_BUILD_WARN),no)
+   CFLAGS += -W -Wall
+else
+   CFLAGS += -Wmissing-braces -Wreturn-type -Wformat -Wno-error=date-time
+   ifneq ($(HB_BUILD_MODE),cpp)
+      CFLAGS += -Wimplicit-int -Wimplicit-function-declaration
+   endif
+endif
+
+# supress the warning:
+# arithmetic on a null pointer treated as a cast from integer to pointer is a GNU extension [-Wgnu-null-pointer-arithmetic]
+CFLAGS += -Wno-gnu-null-pointer-arithmetic
+
+ifneq ($(HB_BUILD_OPTIM),no)
+   CFLAGS += -O3
+endif
+
+ifeq ($(HB_BUILD_DEBUG),yes)
+   CFLAGS += -g
+endif
+
+LD := $(CC)
+LD_OUT := -o
+
+LIBPATHS := $(foreach dir,$(LIB_DIR) $(SYSLIBPATHS),-L$(dir))
+LDLIBS := $(foreach lib,$(HB_USER_LIBS) $(LIBS) $(SYSLIBS),-l$(lib))
+
+# Add the standard C entry
+ifneq ($(HB_LINKING_RTL),)
+   ifeq ($(HB_MAIN),)
+      LDLIBS += -lhbmainstd
+   endif
+endif
+
+LDFLAGS += $(LIBPATHS)
+
+#AR := $(HB_CCPREFIX)ar
+AR := zig ar
+
+# NOTE: The empty line directly before 'endef' HAS TO exist!
+define library_object
+   @$(ECHO) $(ECHOQUOTE)$(subst \,/,$(file))$(ECHOQUOTE) >> __lib__.tmp
+
+endef
+define create_library
+   $(if $(wildcard __lib__.tmp),@$(RM) __lib__.tmp,)
+   $(foreach file,$^,$(library_object))
+   ( $(AR) $(ARFLAGS) $(HB_AFLAGS) $(HB_USER_AFLAGS) rcs $(LIB_DIR)/$@ @__lib__.tmp $(ARSTRIP) ) || ( $(RM) $(subst /,$(DIRSEP),$(LIB_DIR)/$@) && $(FALSE) )
+endef
+
+AR_RULE = $(create_library)
+
+DY := $(CC)
+DFLAGS += -shared $(LIBPATHS)
+DY_OUT := -o$(subst x,x, )
+DLIBS := $(foreach lib,$(HB_USER_LIBS) $(LIBS) $(3RDLIBS) $(SYSLIBS),-l$(lib))
+
+# NOTE: The empty line directly before 'endef' HAS TO exist!
+define dynlib_object
+   @$(ECHO) $(ECHOQUOTE)INPUT($(subst \,/,$(file)))$(ECHOQUOTE) >> __dyn__.tmp
+
+endef
+define create_dynlib
+   $(if $(wildcard __dyn__.tmp),@$(RM) __dyn__.tmp,)
+   $(foreach file,$^,$(dynlib_object))
+   $(DY) $(DFLAGS) -Wl,-soname,$(DYN_NAME_CPT) $(HB_USER_DFLAGS) $(DY_OUT)$(DYN_DIR)/$@ __dyn__.tmp $(DEF_FILE) $(DLIBS) -Wl,--out-implib,$(IMP_FILE),--output-def,$(DYN_DIR)/$(basename $@).def $(DYSTRIP)
+endef
+
+DY_RULE = $(create_dynlib)
+
+#DY_RULE = $(DY) $(DFLAGS) -Wl,-soname,$(DYN_NAME_CPT) $(HB_USER_DFLAGS) $(DY_OUT)$(DYN_DIR)/$@ $^ $(DLIBS) $(DYSTRIP) && $(LN) $(@F) $(DYN_FILE_NVR) && $(LN) $(@F) $(DYN_FILE_CPT)
+
+include $(TOP)$(ROOT)config/rules.mk
